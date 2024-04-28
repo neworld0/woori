777cee44993b209c08b49f0fc329a502c14f21da
Peter JH. Lee <neworld0@gmail.com>
2024-04-29 08:38:42 +0900

    domain add

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,4 +1,8 @@
 from .base import *
 
 
-ALLOWED_HOSTS = ['44.197.21.156']
+ALLOWED_HOSTS = ['44.197.21.156', 'woori-life.co.kr']
+
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = []
+DEBUG = False
